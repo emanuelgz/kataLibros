59943dec55198512820a4fd1dbac2ad9ee4844ee
emanuelgz <egimenez@BFNB12190.baunet.local>
2014-06-10 13:55:40 -0300

    primeros tests

--- test/dojo/KataTest.java
+++ test/dojo/KataTest.java
@@ -1,6 +1,13 @@
 package dojo;
 
+import mockit.Expectations;
+import mockit.Mocked;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import dojo.service.IlegalQuantityException;
+import dojo.service.LibroService;
 
 /*
  * Hay una saga que tiene 5 libros en total. Como está de moda, existe una promoción por comprar los distintos libros de la saga
@@ -22,9 +29,61 @@ import org.junit.Test;
  */
 
 public class KataTest {
-	
-	@Test
-	public void test() {
+
+	@Mocked
+	LibroService libroService;
+
+	Double calcularPrecioTotal(Integer[] libros, final LibroService libroService) throws IlegalQuantityException {
+		double price = libroService.bookPrice() * libros.length;
 		
+		return price - price * libroService.discount(libros.length)/100;
+	}
+
+	@Test
+	public void testGetBookPrice()
+			throws IlegalQuantityException {
+		new Expectations() {{
+			libroService.bookPrice();
+			result = 10D;
+			libroService.discount(1);
+			result = 0;
+			
+		}};
+
+		Double bookPrice = calcularPrecioTotal(new Integer[] { 1 },
+				libroService);
+		Assert.assertEquals(new Double(10D), bookPrice);
+	}
+
+	@Test
+	public void testGetBookPriceTwoBooks()
+			throws IlegalQuantityException {
+		new Expectations() {{
+			libroService.bookPrice();
+			result = 10D;
+			libroService.discount(2);
+			result = 10;
+			
+		}};
+
+		Double bookPrice = calcularPrecioTotal(new Integer[] { 2, 2 },
+				libroService);
+		Assert.assertEquals(new Double(20D), bookPrice);
+	}
+
+	@Test
+	public void testGetBookPriceTwoDifferentBooksWithDiscount()
+			throws IlegalQuantityException {
+		new Expectations() {{
+			libroService.bookPrice();
+			result = 10D;
+			libroService.discount(2);
+			result = 10;
+			
+		}};
+		Double bookPrice = calcularPrecioTotal(new Integer[] { 1, 2 },
+				libroService);
+		Assert.assertEquals(new Double(18D), bookPrice);
+
 	}
 }
